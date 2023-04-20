1713b548f4c15a92c66a0ecfe27ca08f3e233bac
Shubhamkumardon <118193554+Shubhamkumardon@users.noreply.github.com>
2023-04-20 13:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello, Bot By ToonsWood'
+   return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
 os.system("git clone https://github.com/Shubhamkumardon/urlbv5 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
